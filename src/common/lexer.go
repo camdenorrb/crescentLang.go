25013bd59548fce5e300f1f2ff8998e9ca3fd14e
camdenorrb <me@12oclock.dev>
2022-09-20 04:25:16 -0500

    Working lexer????

--- src/common/lexer.go
+++ src/common/lexer.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"errors"
 	"github.com/joomcode/errorx"
 	"io"
 	"strings"
@@ -94,19 +95,22 @@ func (b *tokenBuilder) unsetMode() error {
 	case IdentifierBuilderMode:
 		token.Type = b.syntax.IdentifierTokenType.Unwrap()
 
-	case SymbolBuilderMode:
-		tokenType, exists := b.syntax.tokenTypes[b.cache.String()]
-		if !exists {
-			return errorx.IllegalState.New("Could not find matching symbol type for: %q", b.cache.String())
-		}
-		token.Type = tokenType
-
 	case CommentBuilderMode:
 		token.Type = b.syntax.CommentTokenType.Unwrap()
 
 	case MultiLineCommentBuilderMode:
 		token.Type = b.syntax.MultiLineCommentTokenType.Unwrap()
 
+	case SymbolBuilderMode:
+		tokens, remaining := b.findSymbols()
+		if remaining != "" {
+			return errorx.IllegalState.New("Could not find matching symbol type for: %q", remaining)
+		}
+
+		b.mode = UnsetBuilderMode
+		b.tokens = append(b.tokens, tokens...)
+		b.cache.Reset()
+		return nil
 	}
 
 	b.mode = UnsetBuilderMode
@@ -144,10 +148,15 @@ func (b *tokenBuilder) findSymbols() ([]Token, string) {
 	start := 0
 	current := cacheAsString
 
-	for current != "" {
-		for takeUntil := len(current); takeUntil > 0; takeUntil-- {
+	foundToken := true
 
-			if tokenType, exists := b.syntax.tokenTypes[cacheAsString]; exists {
+	for foundToken {
+
+		foundToken = false
+
+		for takeUntil := len(current) - 1; takeUntil >= 0; takeUntil-- {
+
+			if tokenType, exists := b.syntax.tokenTypes[current]; exists {
 
 				tokens = append(tokens, Token{
 					ColumnRange: IntRange{
@@ -157,12 +166,17 @@ func (b *tokenBuilder) findSymbols() ([]Token, string) {
 					Type: tokenType,
 				})
 
-				current = current[:]
+				start += takeUntil + 1
+				current = cacheAsString[start:]
+				foundToken = true
+				continue
 			}
 
+			current = current[:takeUntil]
 		}
 	}
 
+	return tokens, current
 }
 
 func (b *tokenBuilder) step(character rune) error {
@@ -306,256 +320,3 @@ func (l *GenericLexer) lex(reader *strings.Reader) ([]Token, error) {
 
 	return builder.tokens, nil
 }
-
-/*
-func (l *GenericLexer) lex() error {
-
-	var builder strings.Builder
-
-	for {
-
-		character, _, err := l.reader.ReadRune()
-		if err != nil {
-
-			if err == io.EOF {
-				break
-			}
-
-			return errorx.Decorate(err, "lex failed to read rune")
-		}
-
-		columnIndex++
-
-		switch character {
-
-		case '\n':
-			lineNumber++
-			columnIndex = 0
-
-		case '\'':
-			l.builderMode = CharBuilderMode
-
-		case '"':
-			l.builderMode = StringBuilderMode
-
-			builder.WriteRune(character)
-		}
-
-	}
-
-	// Plan: If syntax doesn't contain Rune, append and check again
-	// Make sure to check if number, string literal, etc
-
-	// TODO: On new line stop comment builder type
-	// TODO: Make a switch for builder type that will automagically save token for last type
-	func(l *GenericLexer) lexLine(line
-	string, lineNumber
-	uint) ([]Token, error) {
-
-		var tokens []Token
-
-		var builder strings.Builder
-
-		for index, character := range []rune(line) {
-
-			if builder.Len() != 0 && (builderMode == IdentifierBuilderMode || builderMode == SymbolBuilderMode) {
-				// If matching token type is found, store token
-				if tokenType, exists := l.syntax.tokenTypes[builder.String()]; exists {
-
-					tokens = append(tokens, Token{
-						ColumnRange: IntRange{Start: index - builder.Len(), End: index},
-						LineNumber:  lineNumber,
-						Type:        tokenType,
-					})
-
-					builder.Reset()
-					builderMode = UnsetBuilderMode
-				} else if builderMode == IdentifierBuilderMode {
-
-					if unicode.IsLetter(character) {
-						builder.WriteRune(character)
-						continue
-					}
-
-					tokens = append(tokens, Token{
-						Value:       builder.String(),
-						ColumnRange: IntRange{Start: index - builder.Len(), End: index},
-						LineNumber:  lineNumber,
-						Type:        l.syntax.IdentifierTokenType.Unwrap(),
-					})
-
-					builder.Reset()
-					builderMode = UnsetBuilderMode
-				}
-			}
-
-			switch character {
-
-			case '"':
-				if builderMode == StringBuilderMode {
-
-					tokens = append(tokens, Token{
-						Value:       builder.String(),
-						ColumnRange: IntRange{Start: index - builder.Len() - len("\""), End: index},
-						LineNumber:  lineNumber,
-						Type:        l.syntax.StringTokenType.Unwrap(),
-					})
-
-					builder.Reset()
-					builderMode = UnsetBuilderMode
-				} else {
-					builderMode = StringBuilderMode
-				}
-
-			case '\'':
-				if builderMode == CharBuilderMode {
-
-					tokens = append(tokens, Token{
-						Value:       builder.String(),
-						ColumnRange: IntRange{Start: index - builder.Len() - len("'"), End: index},
-						LineNumber:  lineNumber,
-						Type:        l.syntax.CharTokenType.Unwrap(),
-					})
-
-					builder.Reset()
-					builderMode = UnsetBuilderMode
-				} else {
-					builderMode = CharBuilderMode
-				}
-
-			// Note: Negative numbers won't be lexed entirely, will be Minus token first
-			case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0':
-
-				// If dot alone it should be originally be parsed as a symbol, but if number present, change
-				if builderMode != UnsetBuilderMode && builder.String() != "." {
-					return nil, errorx.IllegalState.New("invalid number builder %q lineNumber: %d, column: %d", builder.String(), lineNumber, index)
-				}
-
-				builder.WriteRune(character)
-				builderMode = NumberBuilderMode
-
-			default:
-
-				switch builderMode {
-
-				case IdentifierBuilderMode:
-					if !unicode.IsLetter(character) {
-						return nil, errorx.IllegalState.New("invalid keyword character %q lineNumber: %d, column: %d", character, lineNumber, index)
-					}
-					builder.WriteRune(character)
-
-				case SymbolBuilderMode:
-					if unicode.IsLetter(character) || unicode.IsDigit(character) {
-						return nil, errorx.IllegalState.New("invalid symbol character %q lineNumber: %d, column: %d", character, lineNumber, index)
-					}
-					builder.WriteRune(character)
-
-				case StringBuilderMode:
-					builder.WriteRune(character)
-
-				case UnsetBuilderMode:
-					if unicode.IsLetter(character) {
-						builder.WriteRune(character)
-						builderMode = IdentifierBuilderMode
-					} else {
-						builder.WriteRune(character)
-						builderMode = SymbolBuilderMode
-					}
-
-				case NumberBuilderMode:
-
-					if unicode.ToLower(character) != 'f' {
-						return nil, errorx.IllegalState.New("invalid number character %q lineNumber: %d, column: %d", character, lineNumber, index)
-					}
-
-					asFloat, err := strconv.ParseFloat(builder.String(), 32)
-					if err != nil {
-						return nil, errorx.IllegalState.New("invalid number builder %q lineNumber: %d, column: %d", builder.String(), lineNumber, index)
-					}
-
-					tokens = append(tokens, Token{
-						Value:       float32(asFloat),
-						ColumnRange: IntRange{Start: builder.Len() - index, End: index},
-						LineNumber:  lineNumber,
-						Type:        l.syntax.NumberTokenType.Unwrap(),
-					})
-
-					builder.Reset()
-					builderMode = UnsetBuilderMode
-				}
-			}
-		}
-
-		// Must be identifier
-		if builder.Len() > 0 {
-
-			builderAsString := builder.String()
-
-			if tokenType, ok := l.syntax.tokenTypes[builderAsString]; ok {
-
-				tokens = append(tokens, Token{
-					ColumnRange: IntRange{Start: len(line) - builder.Len() - 1, End: len(line) - 1},
-					LineNumber:  lineNumber,
-					Type:        tokenType,
-				})
-
-			} else {
-				switch builderMode {
-
-				// TODO: Switch between types, maybe have a function for handling each one that can be reused in default case
-				case NumberBuilderMode:
-
-					asFloat, err := strconv.ParseFloat(builderAsString, 64)
-					if err != nil {
-						return nil, errorx.IllegalState.New("invalid number builder %q lineNumber: %d, column: %d", builder.String(), lineNumber, len(line)-builder.Len()-1)
-					}
-
-					tokens = append(tokens, Token{
-						Value:       asFloat,
-						ColumnRange: IntRange{Start: len(line) - builder.Len() - 1, End: len(line) - 1},
-						LineNumber:  lineNumber,
-						Type:        l.syntax.NumberTokenType.Unwrap(),
-					})
-
-				case IdentifierBuilderMode:
-					tokens = append(tokens, Token{
-						Value:       builderAsString,
-						ColumnRange: IntRange{Start: len(line) - builder.Len() - 1, End: len(line) - 1},
-						LineNumber:  lineNumber,
-						Type:        l.syntax.IdentifierTokenType.Unwrap(),
-					})
-
-				default:
-					return nil, errorx.IllegalState.New("unexpected builder type %q for %q", builderMode, builderAsString)
-
-				}
-			}
-
-			builder.Reset()
-			builderMode = UnsetBuilderMode
-		}
-
-		return tokens, nil
-	}
-
-	/*
-
-	   func (l *GenericLexer) Lex() ([]Token, error) {
-
-	   	builder := ""
-
-	   	for {
-
-	   		rune, _, err := l.reader.ReadRune()
-	   		if err != nil {
-
-	   			if err != io.EOF {
-	   				return nil, err
-	   			}
-
-	   			break
-	   		}
-
-	   	}
-	   }
-*/
